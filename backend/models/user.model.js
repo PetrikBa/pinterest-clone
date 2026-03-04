b8229869198464459e87e19f639fecd8b5e52b0e
Peter Baca <petrik.baca@gmail.com>
2026-03-04 15:54:54 +0100

    upravy gitignore + pokracovanie prac na BE

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,14 +4,17 @@ const userSchema = new Schema({
     displayName: {
         type: String,
         required: true,
+        unique: true,
     },
     userName: {
         type: String,
         required: true,
+        unique: true,
     },
     email: {
         type: String,
         required: true,
+        unique: true,
     },
     img: {
         type: String,
